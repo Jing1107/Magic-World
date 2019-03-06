fd4369c30b22e5147fb6e51bcd57d8c4c8b201b9
jing.dong <dj3386083@gmail.com>
2019-03-06 14:12:21 +1100

    first commit

---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/Hermione.jpg",
+    price: 42.95,
+    company: "GOOGLE",
+    info:
+      "This replica of Hermione's wand is an authentic recreation of the young wizard's wand featured in the movie Harry Potter and the Prisoner of Azkaban. It comes complete with a replica of the original Ollivanders wand box, featured in the film Harry Potter and the Sorcerer's Stone. Measures 15 inches in length. Made of resin.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 42.95,
+    company: "SAMSUNG",
+    info:
+      "This replica of Harry Potter's wand is an authentic recreation of the famous wizard's wand featured in the movie Harry Potter and the Prisoner of Azkaban. Harry's wand comes complete with a replica of the original Ollivanders wand box, featured in the film Harry Potter and the Sorcerer's Stone. Measures 14 inches in length. Made of resin.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 42.95,
+    company: "htc",
+    info:
+      "This replica of Dumbledore's wand is an authentic recreation of the wizard's wand featured in the Harry Potter movies. Made of resin, the wand is 15 inches long and comes complete with a replica of the wand box as featured in the film Harry Potter and the Sorcerer's Stone.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "This replica of Professor Snape's wand is an authentic recreation of the mysterious wizard's wand featured in the Harry Potter movies. It comes complete with a replica of the original Ollivanders wand box as featured in the film Harry Potter and the Sorcerer's Stone. The wand is 15 inches in length long and is made of resin",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "This high quality, hand-painted wand replica is an authentic recreation of Sirius Black's wand from the films. It comes complete with a replica of the original Ollivanders wand box. Measures 15 inches in length. Made of resin.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "This replica of Draco Malfoy's wand is an authentic recreation of the student's wand featured in the Harry Potter movies. It comes complete with a replica of Ollivanders wand box. Made of resin, the wand is 15 inches long.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 39.95,
+    company: "apple",
+    info:
+      "This high quality, hand-painted wand replica is an authentic recreation of Ginny Weasley's wand from the films. Ginny's wand is dark and smooth with a spiral design at the handle. With detailed sculpture and design, this wand is a perfect addition for both seasoned collectors as well as newer fans. Made of resin, this wand measures approximately 14 inches long and comes with a sleek collector box with name clip.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 42.95,
+    company: "apple",
+    info:
+      "This replica of Ron Weasley's wand is an authentic recreation of the young wizard's wand featured in the movie Harry Potter and the Prisoner of Azkaban. It comes complete with a replica of the original Ollivanders wand box, featured in the film Harry Potter and the Sorcerer's Stone. Measures 14 inches in length. Made of resin.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
